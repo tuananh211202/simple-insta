c1a51f4cd0e3aab990c11fc68053465f0adbfcbe
anhnt2112 <anh.nt211202@gmail.com>
2023-12-18 01:57:04 +0700

    Config for guess route

--- packages/frontend/src/routes/guess-routes.tsx
+++ packages/frontend/src/routes/guess-routes.tsx
@@ -2,8 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import ForgotPassword from "../pages/ForgotPassword";
+import Cookies from 'js-cookie';
 
 const GuessRoutes = () => {
+  const isLogin = Cookies.get('accessToken');
+  if(isLogin) return <></>;
   return (
     <Routes>
       <Route path='/signin' element={<SignIn />} />
