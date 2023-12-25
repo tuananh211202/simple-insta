d62e6cd92081197213684267ec4dc622e1b6c34d
anhnt2112 <anh.nt211202@gmail.com>
2023-12-25 17:18:44 +0700

    Pagination API

--- packages/frontend/src/midleware/api/user.ts
+++ packages/frontend/src/midleware/api/user.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { BASE_URL, PaginationOptions } from "./constants";
 
-export const getListUser = async (name: string, pagOpts: PaginationOptions) => {
-  const query = `page=${pagOpts.page}&pageSize=${pagOpts.pageSize}&name=${name}`;
+export const getListUser = async (filterOptions: { name: string } & PaginationOptions) => {
+  const query = `page=${filterOptions.page}&pageSize=${filterOptions.pageSize}&name=${filterOptions.name}`;
   const response = await axios.get(`${BASE_URL}/user/list?${query}`);
   return response.data;
 }
